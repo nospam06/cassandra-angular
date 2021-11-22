f07ae5a9cae98f9c63537e89163ae4252650214c
no spam <no.spam.period@gmail.com>
2021-11-22 09:45:51 -0800

    session connection

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { KeyspaceComponent } from './keyspace/keyspace.component';
 import { SessionComponent } from './session/session.component';
+import { TableComponent } from './table/table.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/session', pathMatch: 'full' },
-  { path: 'session', component: SessionComponent }];
+  { path: 'session', component: SessionComponent },
+  { path: 'keyspace', component: KeyspaceComponent },
+  { path: 'table', component: TableComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/session/session.component.ts
+++ src/app/session/session.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BackendService } from '../backend/backend.service';
+import { SessionRequest } from '../data/session-request';
+import { SessionResponse } from '../data/session-response';
 
 @Component({
   selector: 'app-session',
@@ -6,10 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./session.component.css']
 })
 export class SessionComponent implements OnInit {
+  sessionRequest: SessionRequest = {url: "localhost", port: 9042};
+  sessionResponse: SessionResponse | undefined;
 
-  constructor() { }
+  constructor(private backendService: BackendService) { }
 
   ngOnInit(): void {
   }
 
+  login(): void {
+    this.backendService.login(this.sessionRequest)
+      .subscribe(r => this.sessionResponse = r);
+  }
 }
